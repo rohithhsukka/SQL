0617d3318d7f0c494ffb60556e267cc2201c3243
rohithsukka <rohithsukka8@gmail.com>
2024-09-03 00:40:22 +0530

    joins

---
+++ SQLQuery18.sql
@@ -0,0 +1,15 @@
+use Human_Resources
+
+select * from dbo.Employees
+
+select * from dbo.Gender
+
+select
+[emp_no],
+[birth_date],
+[first_name],
+[gender_name],
+emp.gender
+from 
+Employees emp inner join 
+Gender gen on gen.gender = emp.gender
